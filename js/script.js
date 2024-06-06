8a4345e18e0537cd9581ea9487704e968497095b
al-ameen <muhdabdullahi36@gmail.com>
2024-06-06 23:22:58 +0100

    animate resource info in

--- js/script.js
+++ js/script.js
@@ -12,5 +12,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
   });
   tl.fromTo("header", { opacity: 1 }, { opacity: 0, delay: 8 });
   tl.to("main", { opacity: 1 });
-  tl.fromTo(".descrition", { opacity: 0 }, { opacity: 1, delay: 1 });
+  tl.fromTo(".descrition", { opacity: 0 }, { opacity: 1 });
+  tl.to("#intro_container", { opacity: 0, display: "none", delay: 8 });
+  tl.fromTo("#resource_info", { opacity: 0 }, { opacity: 1, delay: 0.3 });
 });

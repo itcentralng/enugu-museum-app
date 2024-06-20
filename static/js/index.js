b2445ba571255892eafc548a11728d6812b949a5
al-ameen <muhdabdullahi36@gmail.com>
2024-06-20 20:07:08 +0100

    add text labels to LGAs

--- static/js/index.js
+++ static/js/index.js
@@ -83,26 +83,37 @@ fetch("static/data/map-data.geojson")
         (area) => area.properties.shape2 == areaName
       );
 
+      if (!area) {
+        console.log(`Area ${areaName} not found`);
+        return;
+      }
+
       const ifExists = !map.getLayer(area.properties.shape2);
       if (ifExists) {
+        map.addSource(area.properties.shape2, {
+          type: "geojson",
+          data: area,
+        });
+
         map.addLayer({
           id: area.properties.shape2,
           type: "fill",
           source: area.properties.shape2, // reference the data source
           layout: {},
           paint: {
-            "fill-color": "#964B00", // blue color fill
-            "fill-opacity": 0.7, // 100% opaque
-            "fill-opacity-transition": { duration: 300 }, // 300 milliseconds = 1/2 seconds
+            "fill-color": "#964B00", // brown color fill
+            "fill-opacity": 0.7, // 70% opaque
+            "fill-opacity-transition": { duration: 300 }, // 300 milliseconds = 0.3 seconds
           },
         });
+
         map.addLayer({
           id: area.properties.shape2 + "-outline",
           type: "line",
           source: area.properties.shape2,
           layout: {},
           paint: {
-            "line-color": "rgba(0, 0, 0, .7)",
+            "line-color": "rgba(0, 0, 0, 0.7)",
             "line-width": 2,
           },
         });
@@ -111,7 +122,7 @@ fetch("static/data/map-data.geojson")
         map.setPaintProperty(
           areaName + "-outline",
           "line-color",
-          "rgba(0, 0, 0, .5)"
+          "rgba(0, 0, 0, 0.5)"
         );
       }
     };
@@ -125,23 +136,6 @@ fetch("static/data/map-data.geojson")
         "rgba(0, 0, 0, 0)"
       );
     };
-
-    // Setup map Sources for each area
-    function getCentroid(geometry) {
-      let centroid = [0, 0];
-      let coordinates = geometry.coordinates[0];
-      let numPoints = coordinates.length;
-
-      coordinates.forEach((coord) => {
-        centroid[0] += coord[0];
-        centroid[1] += coord[1];
-      });
-
-      centroid[0] /= numPoints;
-      centroid[1] /= numPoints;
-
-      return centroid;
-    }
 
     // globe Rotation
     let animationId;
@@ -167,7 +161,115 @@ fetch("static/data/map-data.geojson")
       }
     };
 
+    const labelCoords = {
+      Udenu: { name: "Udenu", coord: [7.568128414794562, 6.902689158977569] },
+      "Enugu South": {
+        name: "Enugu South",
+        coord: [7.5139944336961639, 6.410654010484201],
+      },
+      "Nkanu East": {
+        name: "Nkanu East",
+        coord: [7.660996907781123, 6.439308957642309],
+      },
+      "Igbo Eze North": {
+        name: "Igbo Eze North",
+        coord: [7.502215137368748, 7.04],
+      },
+      "Igbo Eze South": {
+        name: "Igbo Eze South",
+        coord: [7.37777, 6.98768802],
+      },
+      Nsukka: { name: "Nsukka", coord: [7.4, 6.85] },
+      "Nkanu West": {
+        name: "Nkanu West",
+        coord: [7.546996907781123, 6.339608957642309],
+      },
+      "Oji River": {
+        name: "Oji River",
+        coord: [7.290038024335388, 6.178287095073438],
+      },
+      "Isi-Uzo": {
+        name: "Isi-Uzo",
+        coord: [7.70438512507352, 6.780977921394026],
+      },
+      Aninri: { name: "Aninri", coord: [7.588673691038051, 6.074119154875143] },
+      "Uzo-Uwani": { name: "Uzo-Uwani", coord: [7.15, 6.8] },
+      "Enugu East": {
+        name: "Enugu East",
+        coord: [7.5139944336961639, 6.570054010484201],
+      },
+      "Igbo Etiti": {
+        name: "Igbo Etiti",
+        coord: [7.401661465903976, 6.736747111090381],
+      },
+      Udi: { name: "Udi", coord: [7.33337682687978, 6.6006787775835246] },
+      "Enugu North": {
+        name: "Enugu North",
+        coord: [7.5139944336961639, 6.470254010484201],
+      },
+      Awgu: { name: "Awgu", coord: [7.448709255727611, 6.217233643018233] },
+      Ezeagu: { name: "Ezeagu", coord: [7.200038024335388, 6.438287095073438] },
+    };
+
+    // Function to add all labels to the map
+    function addAllTextToMap() {
+      for (const key in labelCoords) {
+        if (labelCoords.hasOwnProperty(key)) {
+          const lga = labelCoords[key];
+          const coords = lga.coord;
+          const text = lga.name;
+
+          const sourceId = `point-${coords.join("-")}`;
+          const layerId = `layer-${coords.join("-")}`;
+
+          // Add source if it doesn't exist
+          if (!map.getSource(sourceId)) {
+            map.addSource(sourceId, {
+              type: "geojson",
+              data: {
+                type: "FeatureCollection",
+                features: [
+                  {
+                    type: "Feature",
+                    geometry: {
+                      type: "Point",
+                      coordinates: coords, // Coordinates of the point
+                    },
+                    properties: {
+                      title: text, // Text to display
+                    },
+                  },
+                ],
+              },
+            });
+          }
+
+          // Add a symbol layer to display the text
+          if (!map.getLayer(layerId)) {
+            map.addLayer({
+              id: layerId,
+              type: "symbol",
+              source: sourceId,
+              layout: {
+                "text-field": ["get", "title"], // Use the 'title' property for the text
+                "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
+                "text-offset": [0, 0.6],
+                "text-anchor": "top",
+              },
+              paint: {
+                "text-color": "#000", // Set the text color
+              },
+            });
+          }
+        }
+      }
+    }
+
+    // Call the function to add all labels to the map
+
     map.on("load", () => {
+      addAllTextToMap();
+      highlightAllAreas();
       beginRotation();
       areas.features.forEach((area) => {
         const ifExists = map.getSource(area.properties.shape2);
@@ -177,41 +279,6 @@ fetch("static/data/map-data.geojson")
             data: area,
           });
         }
-      });
-
-      // Add local government labels
-      const labels = {
-        type: "FeatureCollection",
-        features: areas.features.map((area) => ({
-          type: "Feature",
-          geometry: {
-            type: "Point",
-            coordinates: getCentroid(area.geometry),
-          },
-          properties: {
-            title: area.properties.shape2,
-          },
-        })),
-      };
-
-      map.addSource("lga-labels", {
-        type: "geojson",
-        data: labels,
-      });
-
-      map.addLayer({
-        id: "lga-labels",
-        type: "symbol",
-        source: "lga-labels",
-        layout: {
-          "text-field": ["get", "title"],
-          "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
-          "text-offset": [0, 0.6],
-          "text-anchor": "top",
-        },
-        paint: {
-          "text-color": "#000000",
-        },
       });
     });
 

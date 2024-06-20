87e9abcbbbd804e57a73a81a886430eabcce69b5
al-ameen <muhdabdullahi36@gmail.com>
2024-06-20 09:38:53 +0100

    set resource coords for zoomTo effect

--- static/js/index.js
+++ static/js/index.js
@@ -1,25 +1,63 @@
 fetch("static/data/map-data.geojson")
   .then((response) => response.json())
-  .then((lgas) => {
+  .then((areas) => {
     mapboxgl.accessToken =
       "pk.eyJ1IjoiYW1lZW51IiwiYSI6ImNrOTFwcHdlYjAwOGczbmt5Mzk1eHBoNDYifQ.BwOWHvAtshdRUF--Y4kimQ";
 
+    // Init Map
     const map = new mapboxgl.Map({
       container: "map", // container ID
       // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
       style: "mapbox://styles/mapbox/outdoors-v12", // style URL
       center: [7.46275, 6.499083], // starting position
-      zoom: 8.9, // starting zoom
-      attributionControl: false
+      zoom: 8.7, // starting zoom
+      attributionControl: false,
     });
 
-    const selectArea = (areaName) => {
-      const area = lgas.features.find(
-        (lga) => lga.properties.shape2 == areaName
+    // Resource coords & zoom levels
+    const coords = {
+      Sandstone: { x: 7.3, y: 6.3, zoom: 9 },
+      Coal: { x: 7, y: 6.5, zoom: 8.9 },
+      "Oil Shale": { x: 7.2, y: 6.25, zoom: 9 },
+      Gas: { x: 6.9, y: 6.7, zoom: 9 },
+      Ironstone: { x: 6.9, y: 6.5, zoom: 8.7 },
+      "Clay Minerals": { x: 7.2, y: 6.5, zoom: 9 },
+      Limestone: { x: 7.2, y: 6.3, zoom: 9 },
+      Gypsum: { x: 6.8, y: 6.7, zoom: 9.2 },
+      Alum: { x: 7.35, y: 6.3, zoom: 10.2 },
+      Alluvium: { x: 7.35, y: 6.15, zoom: 10 },
+      Kaolin: { x: 6.93, y: 6.65, zoom: 9 },
+      "Lead & Zinc": { x: 7.3, y: 6.13, zoom: 9.4 },
+      Granite: { x: 7.4, y: 6.3, zoom: 9.1 },
+      "Iron Ore": { x: 7.2, y: 6.45, zoom: 9.4 },
+      Clay: { x: 7.43, y: 6, zoom: 9.6 },
+    };
+
+    // Zoom to Resource
+    const zoomToResource = (resourceName) => {
+      const resource = coords[resourceName];
+      if (resource) {
+        map.flyTo({
+          center: [resource.x, resource.y],
+          speed: 0.2,
+          zoom: resource.zoom,
+        });
+      } else {
+        console.error(`Resource ${resourceName} not found.`);
+      }
+    };
+
+    const zoomOut = () => {
+      map.flyTo({ center: [7.46275, 6.499083], speed: 0.2, zoom: 8.9 });
+    };
+
+    // Highlight Resource Areas
+    const highlightArea = (areaName) => {
+      const area = areas.features.find(
+        (area) => area.properties.shape2 == areaName
       );
 
       const ifExists = !map.getLayer(area.properties.shape2);
-      console.log(ifExists);
       if (ifExists) {
         map.addLayer({
           id: area.properties.shape2,
@@ -27,8 +65,8 @@ fetch("static/data/map-data.geojson")
           source: area.properties.shape2, // reference the data source
           layout: {},
           paint: {
-            "fill-color": "#0080ff", // blue color fill
-            "fill-opacity": 1, // 100% opaque
+            "fill-color": "#964B00", // blue color fill
+            "fill-opacity": 0.7, // 100% opaque
             "fill-opacity-transition": { duration: 300 }, // 300 milliseconds = 1/2 seconds
           },
         });
@@ -38,19 +76,21 @@ fetch("static/data/map-data.geojson")
           source: area.properties.shape2,
           layout: {},
           paint: {
-            "line-color": "#000",
+            "line-color": "rgba(0, 0, 0, .7)",
             "line-width": 2,
           },
         });
       } else {
-        map.setPaintProperty(areaName, "fill-opacity", 1);
+        map.setPaintProperty(areaName, "fill-opacity", 0.7);
         map.setPaintProperty(
           areaName + "-outline",
           "line-color",
-          "rgba(0, 0, 0, 1)"
+          "rgba(0, 0, 0, .5)"
         );
       }
     };
+
+    // Remove Resource area Highlighting
     const deselectArea = (areaName) => {
       map.setPaintProperty(areaName, "fill-opacity", 0);
       map.setPaintProperty(
@@ -60,10 +100,11 @@ fetch("static/data/map-data.geojson")
       );
     };
 
+    // Setup map Sources for each area
     map.on("load", () => {
-      lgas.features.forEach((area) => {
-        const ifExists = !map.getSource(area.properties.shape2);
-        if (ifExists) {
+      areas.features.forEach((area) => {
+        const ifExists = map.getSource(area.properties.shape2);
+        if (!ifExists) {
           map.addSource(area.properties.shape2, {
             type: "geojson",
             data: area,
@@ -80,14 +121,14 @@ fetch("static/data/map-data.geojson")
       if (data.status != "removed") {
         RFID = data.status;
         const resource = enuguResources.find((resource) => resource.id == RFID);
-        console.log(resource);
-        console.log(RFID);
         selectedLocations = resource.locations;
+        zoomToResource(resource.name);
         resource.locations.forEach((location) => {
-          selectArea(location.name);
+          highlightArea(location.name);
         });
       } else {
         selectedLocations.forEach((location) => {
+          zoomOut();
           deselectArea(location.name);
         });
       }
